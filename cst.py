2e99f13150d8059492a346501062493bf3cfdf8e
Xander Wong <xanxerus@gmail.com>
2017-03-19 17:45:38 -0500

    Committing possible changes?

--- cst.py
+++ cst.py
@@ -3,7 +3,7 @@
 import json
 import sys
 
-def parse(file='cstimer.txt', dest='/home/jbelpois/Desktop'):
+def parse(file='cstimer.txt', dest='/home/xanxerus/Desktop'):
 	contents = json.load(open(file, 'r'))
 	contents = {i:json.loads(contents[i]) for i in contents if i != 'properties'}
 
@@ -15,7 +15,7 @@ def parse(file='cstimer.txt', dest='/home/jbelpois/Desktop'):
 					out.write("%.3f\n" % (i[0][1]*1e-3))
 
 if len(sys.argv) <= 1:
-	parse('cstimer2016-08-09-yellow.txt')
+	parse('31-White.txt')
 else:
 	for f in sys.argv[1:]:
 		parse(f)
